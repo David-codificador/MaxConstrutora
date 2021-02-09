c09456d75f4db49a0071531d085432e9981fa4b7
David-codificador <52103979+David-codificador@users.noreply.github.com>
2021-02-09 10:54:33 -0300

    alterações dia 09-02

--- App/Views/layoutsSite/header.php
+++ App/Views/layoutsSite/header.php
@@ -12,23 +12,22 @@
 
         <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet">
         <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
-        <link rel="stylesheet" type="text/css" href="<?=CSSSITE?>bootstrap.min.css" media="screen">	
-        <link rel="stylesheet" type="text/css" href="<?=CSSSITE?>font-awesome.css" media="screen">
-        <link rel="stylesheet" type="text/css" href="<?=CSSSITE?>jquery.bxslider.css" media="screen">
-        <link rel="stylesheet" type="text/css" href="<?=CSSSITE?>magnific-popup.css" media="screen">
+        <link rel="stylesheet" type="text/css" href="<?= CSSSITE ?>bootstrap.min.css" media="screen">	
+        <link rel="stylesheet" type="text/css" href="<?= CSSSITE ?>font-awesome.css" media="screen">
+        <link rel="stylesheet" type="text/css" href="<?= CSSSITE ?>jquery.bxslider.css" media="screen">
+        <link rel="stylesheet" type="text/css" href="<?= CSSSITE ?>magnific-popup.css" media="screen">
 
         <!-- REVOLUTION STYLE SHEETS -->
-        <link rel="stylesheet" type="text/css" href="<?=CSSSITE?>settings.css">
+        <link rel="stylesheet" type="text/css" href="<?= CSSSITE ?>settings.css">
         <!-- REVOLUTION LAYERS STYLES -->
-        <link rel="stylesheet" type="text/css" href="<?=CSSSITE?>layers.css">
+        <link rel="stylesheet" type="text/css" href="<?= CSSSITE ?>layers.css">
         <!-- REVOLUTION NAVIGATION STYLES -->
-        <link rel="stylesheet" type="text/css" href="<?=CSSSITE?>navigation.css">
+        <link rel="stylesheet" type="text/css" href="<?= CSSSITE ?>navigation.css">
 
-        <link rel="stylesheet" type="text/css" href="<?=CSSSITE?>style.css" media="screen">
+        <link rel="stylesheet" type="text/css" href="<?= CSSSITE ?>style.css" media="screen">
 
 
     </head>
     <body>
-    <input type="hidden" id="link" value="<?= LINK ?>" />
-    <input type="hidden" id="recurso" value="<?= RECURSO ?>" />
-    
+        <input type="hidden" id="link" value="<?= LINK ?>" />
+        <input type="hidden" id="recurso" value="<?= RECURSO ?>" />
