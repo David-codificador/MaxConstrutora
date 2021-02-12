bc0fed72d489036b0c03838e3bd6bd4c2b8b6039
David-codificador <52103979+David-codificador@users.noreply.github.com>
2021-02-12 14:49:30 -0300

    Update sobre.php

--- App/Views/home/sobre.php
+++ App/Views/home/sobre.php
@@ -1,8 +1,16 @@
 <hr>
 <section class="about-alternative-section">
-  
+
     <div class="container">
-        <div class="row">
+        <div class="row">           
+            <div class="col-md-8">
+                <div class="second-article">
+                    <img src="upload/others/3.jpg" alt="">
+                    <h2>Who we are</h2>
+                    <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                    <p>Excepteur sint occaecat cupidatat non proident, sunt</p>
+                </div>
+            </div>   
             <div class="col-md-4">
                 <!-- Nav tabs -->
                 <div class="tab-posts-box">
@@ -37,82 +45,7 @@
                         </div>
                     </div>
                 </div>
-            </div>
-
-            <div class="col-md-4">
-                <div class="accordion-box">
-                    <div class="accord-elem active">
-                        <div class="accord-title">
-                            <a class="accord-link" href="#"></a>
-                            <h2>Vivamus vestibulum nulla nec ante.</h2>
-                        </div>
-                        <div class="accord-content">
-                            <p>Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu. Cras consequat. Praesent dapibus, neque id cursus faucibus, tortor neque.</p>
-                        </div>
-                    </div>
-
-                    <div class="accord-elem">
-                        <div class="accord-title">
-                            <a class="accord-link" href="#"></a>
-                            <h2>Morbi in sem quis dui placerat ornare. </h2>
-                        </div>
-                        <div class="accord-content">
-                            <p>Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu. Cras consequat. Praesent dapibus, neque id cursus faucibus, tortor neque.</p>
-                        </div>
-                    </div>
-
-                    <div class="accord-elem">
-                        <div class="accord-title">
-                            <a class="accord-link" href="#"></a>
-                            <h2>Lorem ipsum dolor sit amet. </h2>
-                        </div>
-                        <div class="accord-content">
-                            <p>Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu. Cras consequat. Praesent dapibus, neque id cursus faucibus, tortor neque.</p>
-                        </div>
-                    </div>
-
-                    <div class="accord-elem">
-                        <div class="accord-title">
-                            <a class="accord-link" href="#"></a>
-                            <h2>Vivamus vestibulum nulla nec ante.</h2>
-                        </div>
-                        <div class="accord-content">
-                            <p>Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu. Cras consequat. Praesent dapibus, neque id cursus faucibus, tortor neque.</p>
-                        </div>
-                    </div>
-
-                    <div class="accord-elem">
-                        <div class="accord-title">
-                            <a class="accord-link" href="#"></a>
-                            <h2>Morbi in sem quis dui placerat ornare. </h2>
-                        </div>
-                        <div class="accord-content">
-                            <p>Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu. Cras consequat. Praesent dapibus, neque id cursus faucibus, tortor neque.</p>
-                        </div>
-                    </div>
-
-                    <div class="accord-elem">
-                        <div class="accord-title">
-                            <a class="accord-link" href="#"></a>
-                            <h2>Lorem ipsum dolor sit amet. </h2>
-                        </div>
-                        <div class="accord-content">
-                            <p>Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu. Cras consequat. Praesent dapibus, neque id cursus faucibus, tortor neque.</p>
-                        </div>
-                    </div>
-
-                </div>
-            </div>
-
-            <div class="col-md-4">
-                <div class="second-article">
-                    <img src="upload/others/3.jpg" alt="">
-                    <h2>Who we are</h2>
-                    <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-                    <p>Excepteur sint occaecat cupidatat non proident, sunt</p>
-                </div>
-            </div>
-
+            </div>          
         </div>
     </div>
 </section>
