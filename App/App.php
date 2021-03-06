506919318a5fd49efb1e95a811916d9612735f93
Igor Justino Rodrigues <igor98rodrigues@gmail.com>
2021-03-06 09:49:02 -0300

    servidor

--- App/App.php
+++ App/App.php
@@ -15,9 +15,9 @@ class App {
 
     public function __construct() {
         define('TITLESITE',          "Maxx");
-        define('APP_HOST',           $_SERVER['HTTP_HOST'] . "/MaxConstrutora");
+        define('APP_HOST',           $_SERVER['HTTP_HOST'] . "");
         define('PATH',               realpath('./'));
-        define('LINK',               'http://' . APP_HOST . '/');
+        define('LINK',               'https://' . APP_HOST . '/');
         define('RECURSO',            LINK . '/public/');
         define('EMAIL',              "maxuel@maxxconstrutora.com.br");
         
@@ -31,10 +31,16 @@ class App {
         define('JSSITE',             RECURSO . 'jsSite/');
         define('IMAGEMSITE',         RECURSO . 'imagemSite/');
 
+        //define('DB_HOST',            "localhost");
+        //define('DB_USER',            "root");
+        //define('DB_PASSWORD',        "");
+        //define('DB_NAME',            "maxconstrutora");
+
         define('DB_HOST',            "localhost");
-        define('DB_USER',            "root");
-        define('DB_PASSWORD',        "");
-        define('DB_NAME',            "maxconstrutora");
+        define('DB_USER',            "maxx_user");
+        define('DB_PASSWORD',        "NLl;HCpcCBB5");
+        define('DB_NAME',            "maxx_construtora_bd");
+		
 
         $this->url();
     }
